2eb455fbd3daf6fc557fdb1a15c46df40b99d130
faltdor <faltdor@gmail.com>
2017-09-07 13:26:30 -0400

    Add final proyect with button to paypal

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -5,10 +5,37 @@ angular.module('templateStore.templates',['ngRoute'])
 		when('/templates',{
 			templateUrl: 'templates/templates.html',
 			controller: 'TemplateCtrl'
-		});
+		}).
+		when('/templates/:templateId',{
+			templateUrl:'templates/template-details.html',
+			controller: 'TemplateDetailsCtrl'
+		})	
+		
 
 }])
 
-.controller('TemplateCtrl',['$scope',function($scope){
+.controller('TemplateCtrl',['$scope','$http',function($scope,$http){
 	console.log('TemplateCtrl');
+	$http.get('../json/templates.json').then(function(response){
+		$scope.templates = response.data;
+	})
+}])
+
+.controller('TemplateDetailsCtrl',['$scope','$http','$routeParams','$filter',
+								  function($scope,$http,$routeParams,$filter){
+	console.log('TemplateDetailsCtrl');
+
+	var templateId = $routeParams.templateId;
+	$http.get('../json/templates.json').then(function(response){
+		$scope.template = $filter('filter')(response.data,function(d){
+			return d.id == templateId;
+		})[0];
+		$scope.mainImage = $scope.template.images[0].name;
+
+	});
+
+	$scope.setImage = function(image){
+		$scope.mainImage = image.name;
+	}
+
 }])
